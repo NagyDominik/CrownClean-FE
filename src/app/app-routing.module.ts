d36f933a26a24a38c900a9ff30bd88ce207e0ca5
Péter Sebők <sebok.peter97@gmail.com>
2018-11-30 10:25:09 +0100

    Added simpla user-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import {  HttpClientModule } from "@angular/common/http";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersListComponent}
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
