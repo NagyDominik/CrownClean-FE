5096125cc149ef0706986fd0006414d169b33acd
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-11-30 13:00:52 +0100

    Flex layout fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersListComponent } from './users/users-list/users-list.component';
 import { UserDetailComponent } from './users/user-detail/user-detail.component';
 import { UserUpdateComponent } from './users/user-update/user-update.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  {path: 'users', component: UsersListComponent},
-  {path: 'users/:id', component: UserDetailComponent},
-  {path: 'user-update/:id', component: UserUpdateComponent}
+  { path: 'admin', component: AdminComponent },
+
+  { path: 'users', component: UsersListComponent },
+  { path: 'users/:id', component: UserDetailComponent },
+  { path: 'user-update/:id', component: UserUpdateComponent },
 ];
 
 @NgModule({
