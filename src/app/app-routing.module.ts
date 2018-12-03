cdb3f3f602bc53281785bfb67a873fdf6d4221d0
Péter Sebők <sebok.peter97@gmail.com>
2018-12-03 10:56:14 +0100

    Added rudamentary vehicle detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { UserUpdateComponent } from './users/user-update/user-update.component';
 import { UserAddComponent } from './users/user-add/user-add.component';
 import { AdminComponent } from './admin/admin.component';
 import { VehiclesListComponent } from './vehicles/vehicles-list/vehicles-list.component';
+import { VehicleDetailComponent } from './vehicles/vehicle-detail/vehicle-detail.component';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent },
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'users/:id', component: UserDetailComponent },
   { path: 'user-update/:id', component: UserUpdateComponent },
   { path: 'user-add', component: UserAddComponent },
-  { path: 'vehicles', component: VehiclesListComponent }
+  { path: 'vehicles', component: VehiclesListComponent },
+  { path: 'vehicles/:id', component: VehicleDetailComponent}
 ];
 
 @NgModule({
--- src/app/vehicles/vehicle-detail/vehicle-detail.component.ts
+++ src/app/vehicles/vehicle-detail/vehicle-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { VehicleService } from 'src/app/shared/services/vehicle_service/vehicle.service';
+import { ActivatedRoute } from '@angular/router';
+import { Vehicle } from 'src/app/shared/models/vehicle';
 
 @Component({
   selector: 'app-vehicle-detail',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vehicleService: VehicleService,
+              private route: ActivatedRoute) { }
+
+  currentVehicle: Vehicle
 
   ngOnInit() {
+    this.getVehicle();
+  }
+
+  getVehicle()
+  {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.vehicleService.getVehicleByID(id).subscribe(vehicle => {
+      this.currentVehicle = vehicle;
+    },
+      error => {
+        console.log(error);
+        alert(error.message);
+      }
+    );
   }
 
 }
