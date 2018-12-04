e605fe8d0d9b7f36f40b4d2272c01c242dafcf47
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-12-04 10:51:55 +0100

    approving a user feature is working

--- src/app/shared/services/user_service/user.service.ts
+++ src/app/shared/services/user_service/user.service.ts
@@ -9,29 +9,31 @@ import { environment } from 'src/environments/environment';
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>( environment.apiURL + 'users');
+    return this.http.get<User[]>(environment.apiURL + 'users');
   }
 
   getUserByID(id: number): Observable<User> {
     return this.http.get<User>(environment.apiURL + 'users/' + id);
   }
 
-  updateUser(user: User): Observable<any>
-  {
+  updateUser(user: User): Observable<any> {
     const id = user.id;
     return this.http.put(environment.apiURL + 'users/' + id, user, {responseType: 'text'});
   }
 
-  addUser(user: User): Observable<any>
-  {
+  approveUser(id: number): Observable<any> {
+    return this.http.put(environment.apiURL + 'users/approve/' + id, '');
+  }
+
+  addUser(user: User): Observable<any> {
     return this.http.post(environment.apiURL + 'users', user, {responseType: 'text'});
   }
 
-  deleteUser(id: number): Observable<any>
-  {
+  deleteUser(id: number): Observable<any> {
     return this.http.delete(environment.apiURL + 'users/' + id, {responseType: 'text'});
   }
 }
