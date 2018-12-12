59d7d37fae2ac9c329552e8051f778ccdc3a9cce
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-12-12 13:09:37 +0100

    vehicle add style

--- src/app/vehicles/vehicle-add/vehicle-add.component.ts
+++ src/app/vehicles/vehicle-add/vehicle-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-add',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleAddComponent implements OnInit {
 
+  newVehicleForm = new FormGroup({
+    Brand: new FormControl(''),
+    UniqueID: new FormControl(''),
+    Type: new FormControl(''),
+    Size: new FormControl(''),
+    InternalPlus: new FormControl(''),
+  });
+
+  typeList: string[] =
+    ['Small',
+    'Midsize',
+    'Large/Wagon',
+    'MPV',
+    'Boat'];
+  vehicleIsBoat = false;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  changeBoatSelection() {
+    this.vehicleIsBoat = !this.vehicleIsBoat;
+    if (this.vehicleIsBoat) {
+      console.log('Vehicle is boat');
+      this.newVehicleForm.get('Size').enable();
+    } else {
+      console.log('Vehicle is not boat');
+      this.newVehicleForm.get('Size').disable();
+    }
+
+  }
+  save() {
+    const vehicle = this.newVehicleForm.value;
   }
 
 }
