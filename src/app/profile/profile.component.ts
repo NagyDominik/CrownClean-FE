40c2c7a99814790a3b0787a80b7466d2adcf847a
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-12-13 14:40:24 +0100

    user profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../shared/services/user_service/user.service';
 import { TokenService } from '../shared/services/token_service/token.service';
 import { User } from '../shared/models/User/user';
 import { MatSnackBar } from '@angular/material';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -12,15 +13,28 @@ import { MatSnackBar } from '@angular/material';
 export class ProfileComponent implements OnInit {
 
   constructor(private userService: UserService,
-              private tokenService: TokenService,
-              private snackBar: MatSnackBar) { }
+    private tokenService: TokenService,
+    private snackBar: MatSnackBar) { }
 
   currentUser: User;
+
+  profileForm = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl('')
+  });
 
   ngOnInit() {
     this.tokenService.getUserFromToken().subscribe(user => {
       this.userService.getUserByID(user.id).subscribe(userById => {
         this.currentUser = userById;
+        this.profileForm.patchValue({
+          firstname: userById.firstName,
+          lastname: userById.lastName,
+          phone: userById.phoneNumber,
+          email: userById.email
+        })
       },
         error => {
           console.log(error);
@@ -31,6 +45,23 @@ export class ProfileComponent implements OnInit {
       error => {
         console.log(error);
         this.openSnackBar(error.message);
+      }
+    );
+  }
+
+  save() {
+    const phone = this.profileForm.get("phone").value;
+    const email = this.profileForm.get("email").value;
+    if (phone != this.currentUser.phoneNumber)
+      this.currentUser.phoneNumber = phone;
+    if (email != this.currentUser.email)
+      this.currentUser.email = email;
+    this.userService.updateUser(this.currentUser).subscribe(() => {
+      this.openSnackBar('Update Succesful!');
+    },
+      error => {
+        console.log(error);
+        alert(error.message);
       }
     );
   }
