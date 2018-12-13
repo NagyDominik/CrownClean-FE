0f915da24a084e9ccfc14be9913deb425a2b1e67
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-12-13 14:20:28 +0100

    vehicle list for currentuser (not working)

--- src/app/vehicles/vehicles-list/vehicles-list.component.ts
+++ src/app/vehicles/vehicles-list/vehicles-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleService } from 'src/app/shared/services/vehicle_service/vehicle.service';
 import { Vehicle } from 'src/app/shared/models/Vehicle/vehicle';
+import {User} from '../../shared/models/User/user';
+import {MatSnackBar} from '@angular/material';
+import {UserService} from '../../shared/services/user_service/user.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FilteredVehicleList} from '../../shared/models/Vehicle/FilteredVehicleList';
 
 @Component({
   selector: 'app-vehicles-list',
@@ -9,17 +14,22 @@ import { Vehicle } from 'src/app/shared/models/Vehicle/vehicle';
 })
 export class VehiclesListComponent implements OnInit {
 
-  constructor(private vehicleService: VehicleService) { }
- 
+  constructor(private vehicleService: VehicleService, public snackBar: MatSnackBar,
+              private userService: UserService, private route: ActivatedRoute, private router: Router,
+              ) { }
+
   vehicles: Vehicle[];
   count: number;
+  
+ list: Vehicle[]
+
 
   ngOnInit() {
     this.refresh();
   }
 
 
-  delete(id: number) 
+  delete(id: number)
   {
     this.vehicleService.deleteVehicle(id).subscribe(() =>{
       this.refresh();
@@ -31,8 +41,7 @@ export class VehiclesListComponent implements OnInit {
     );
   }
 
-  refresh() 
-  {
+  refresh() {
     this.vehicleService.getVehicles().subscribe(vehicleList => {
       this.vehicles = vehicleList.vehicles;
       this.count = vehicleList.count;
@@ -42,6 +51,24 @@ export class VehiclesListComponent implements OnInit {
         alert(error.error);
       }
     );
+  }
+
+  getUser() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUserByID(id).subscribe(user => {
+       this.list = user.vehicles;
+      },
+      err => {
+        console.log(err);
+        this.openSnackBar(err.error);
+      }
+    );
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'OK', {
+      duration: 1500,
+    });
   }
 
 }
