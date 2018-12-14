df74331eed9df7ddd0e409f483434bfb7534bfc5
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-12-14 13:05:45 +0100

    improvements

--- src/app/users/user-update/user-update.component.ts
+++ src/app/users/user-update/user-update.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/shared/services/user_service/user.service';
 import { FormGroup, FormControl } from '@angular/forms';
-import {User} from '../../shared/models/User/user';
-import {MatSnackBar} from '@angular/material';
-import {Location} from '@angular/common';
+import { User } from '../../shared/models/User/user';
+import { MatSnackBar } from '@angular/material';
+import { Location } from '@angular/common';
+import { TokenService } from 'src/app/shared/services/token_service/token.service';
 
 @Component({
   selector: 'app-user-update',
@@ -14,38 +15,44 @@ import {Location} from '@angular/common';
 export class UserUpdateComponent implements OnInit {
 
   constructor(private userService: UserService,
-              private route: ActivatedRoute,
-              private router: Router,
-              public snackBar: MatSnackBar,
-              private location: Location) { }
+    private route: ActivatedRoute,
+    private tokenService: TokenService,
+    public snackBar: MatSnackBar,
+    private location: Location) { }
 
   id: number;
-
-
+  currentUser: User;
+  userUpdate: User;
 
   userForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
     phoneNumber: new FormControl(''),
-    addresses: new FormControl(''),
     isCompany: new FormControl(''),
     isAdmin: new FormControl(''),
-    isApproved: new FormControl('')
   });
 
   ngOnInit() {
     this.id = +this.route.snapshot.paramMap.get('id');
+    this.tokenService.getUserFromToken().subscribe(user => {
+      this.userService.getUserByID(user.id).subscribe(userById => {
+        this.currentUser = userById;
+      })
+    });
+    this.refresh();
+  }
+
+  refresh() {
     this.userService.getUserByID(this.id).subscribe(user => {
+      this.userUpdate = user;
       this.userForm.patchValue({
         firstName: user.firstName,
         lastName: user.lastName,
         email: user.email,
         phoneNumber: user.phoneNumber,
-        addresses: user.addresses,
         isCompany: user.isCompany,
-        isAdmin: user.isAdmin,
-        isApproved: user.isApproved
+        isAdmin: user.isAdmin
       });
     },
       error => {
@@ -64,12 +71,30 @@ export class UserUpdateComponent implements OnInit {
     user.id = this.id;
 
     this.userService.updateUser(user).subscribe(() => {
-      this.back();
+      this.openSnackBar('Update Succesful!');
     },
       error => {
         console.log(error);
         alert(error.message);
       }
     );
+  }
+
+  approve(id: number) {
+    this.userService.approveUser(id).subscribe(() => {
+      this.openSnackBar('User has been approved!');
+      this.refresh();
+    },
+      error => {
+        console.log(error);
+        this.openSnackBar(error.error);
+      }
+    );
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'OK', {
+      duration: 1500,
+    });
   }
 }
